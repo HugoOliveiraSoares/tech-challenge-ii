9fddd5cdc50d926abcbc52432a6263e09a05fa3b
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-02-25 21:40:45 -0300

    create Existing menu item exception

---
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/core/exception/ExistingMenuItemException.java
@@ -0,0 +1,9 @@
+package br.com.fiap.tech_challenge_ii.menu.core.exception;
+
+public class ExistingMenuItemException extends RuntimeException {
+
+    public ExistingMenuItemException(String message) {
+        super(message);
+    }
+
+}
