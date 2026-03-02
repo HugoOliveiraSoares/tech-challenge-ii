c824503a75cae0b10f1de5c7713c9a3cf158b160
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-02 09:41:06 -0300

    active profile

--- src/test/java/br/com/fiap/tech_challenge_ii/TechChallengeIiApplicationTests.java
+++ src/test/java/br/com/fiap/tech_challenge_ii/TechChallengeIiApplicationTests.java
@@ -2,12 +2,14 @@ package br.com.fiap.tech_challenge_ii;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class TechChallengeIiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
