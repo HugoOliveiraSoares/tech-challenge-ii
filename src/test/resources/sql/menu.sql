59a8f086dcfca27bbf4764fa0ac14ce1f5210a1b
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-05 20:31:44 -0300

    remove id from menu.sql

--- src/test/resources/sql/menu.sql
+++ src/test/resources/sql/menu.sql
@@ -1,15 +1,15 @@
-INSERT INTO address (id, street, number, neighborhood, city, zip_code)
-VALUES (1, 'Main St', '123', 'Downtown', 'São Paulo', '01000-000');
+INSERT INTO address (street, number, neighborhood, city, zip_code)
+VALUES ('Main St', '123', 'Downtown', 'São Paulo', '01000-000');
 
-INSERT INTO app_user (id, name, email, login, password, user_type, last_modified_date)
-VALUES (1, 'Test Owner', 'owner@test.com', 'owner', 'password', 'OWNER', CURRENT_DATE);
+INSERT INTO app_user (name, email, login, password, user_type, last_modified_date)
+VALUES ('Test Owner', 'owner@test.com', 'owner', 'password', 'OWNER', CURRENT_DATE);
 
 INSERT INTO restaurant
-(id, name, opening_hours, address_id, kitchen_type, owner_id)
-VALUES(1, 'Test Restaurant', '10h', 1, 'Brazilian', 1);
+(name, opening_hours, address_id, kitchen_type, owner_id)
+VALUES('Test Restaurant', '10h', 1, 'Brazilian', 1);
 
 INSERT INTO 
-    menu_item (id, name, description, price, only_local_consumption, photo_path, restaurant_id)
+    menu_item (name, description, price, only_local_consumption, photo_path, restaurant_id)
 VALUES 
-    (1, 'Feijoada', 'Traditional Brazilian black bean stew with pork', 45.90, false, '/images/feijoada.jpg', 1),
-    (2, 'Caipirinha', 'Classic Brazilian cocktail', 15.00, false, '/images/caipirinha.jpg', 1);
+    ('Feijoada', 'Traditional Brazilian black bean stew with pork', 45.90, false, '/images/feijoada.jpg', 1),
+    ('Caipirinha', 'Classic Brazilian cocktail', 15.00, false, '/images/caipirinha.jpg', 1);
