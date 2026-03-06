487d4c0db49e3d6e020c8ac4e763c40f381df48a
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-06 12:09:06 -0300

    create userNotFoundException

---
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/core/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.fiap.tech_challenge_ii.menu.core.exception;
+
+public class UserNotFoundException extends NotFoundException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
