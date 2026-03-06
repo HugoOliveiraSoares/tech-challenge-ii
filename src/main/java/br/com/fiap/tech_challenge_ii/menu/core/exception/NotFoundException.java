2033c007a4a8134993b49530d996c996c4fe3a59
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-05 21:21:46 -0300

    create NotFoundException and RestaurantException extends it

---
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/core/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.fiap.tech_challenge_ii.menu.core.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/br/com/fiap/tech_challenge_ii/menu/core/exception/RestaurantNotFoundException.java
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/core/exception/RestaurantNotFoundException.java
@@ -1,6 +1,6 @@
 package br.com.fiap.tech_challenge_ii.menu.core.exception;
 
-public class RestaurantNotFoundException extends RuntimeException {
+public class RestaurantNotFoundException extends NotFoundException {
 
     public RestaurantNotFoundException(String message) {
         super(message);
