4aec92d36f0afbc1582c5a7b4e9742e2e8f4dc76
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-05 21:21:57 -0300

    create GlobalExceptionHandler

---
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/infra/controller/GlobalExceptionHandler.java
@@ -0,0 +1,85 @@
+package br.com.fiap.tech_challenge_ii.menu.infra.controller;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.fiap.tech_challenge_ii.menu.core.exception.ExistingMenuItemException;
+import br.com.fiap.tech_challenge_ii.menu.core.exception.NotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ExistingMenuItemException.class)
+    protected ResponseEntity<ProblemDetail> handleExistingMenuItemException(ExistingMenuItemException ex,
+            final WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+        problemDetail.setType(URI.create("https://example.com/bad-request"));
+        problemDetail.setTitle("Existing Menu Item");
+        problemDetail.setInstance(URI.create(
+                request.getDescription(false).replace("uri=", "")));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+
+    }
+
+    @ExceptionHandler(value = { NotFoundException.class })
+    protected ResponseEntity<ProblemDetail> handleNotFound(final NotFoundException ex, final WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setType(URI.create("https://example.com/not-found"));
+        problemDetail.setTitle("Not Found");
+        problemDetail.setInstance(URI.create(
+                request.getDescription(false).replace("uri=", "")));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
+    }
+
+    @ExceptionHandler(value = { Exception.class })
+    protected ResponseEntity<Object> handleGeneralException(final RuntimeException ex, final WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+                "An unexpected error occurred");
+        problemDetail.setType(URI.create("https://example.com/internal-server-error"));
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setInstance(URI.create(request.getDescription(false).replace("uri=", "")));
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problemDetail);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        List<Map<String, String>> invalidParams = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(error -> {
+                    Map<String, String> param = new HashMap<>();
+                    param.put("name", error.getField());
+                    param.put("reason", error.getDefaultMessage());
+                    return param;
+                })
+                .collect(Collectors.toList());
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Validation failed");
+        problemDetail.setType(URI.create("https://example.com/validation-error"));
+        problemDetail.setTitle("Validation Error");
+        problemDetail.setInstance(URI.create(request.getDescription(false).replace("uri=", "")));
+        problemDetail.setProperty("invalid-params", invalidParams);
+        return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
+    }
+}
