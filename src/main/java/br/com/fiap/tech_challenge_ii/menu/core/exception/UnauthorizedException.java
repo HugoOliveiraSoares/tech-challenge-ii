b41930527eb42f7c848004953ec031daae99939e
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-06 09:55:29 -0300

    create UnauthorizedException

---
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/core/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package br.com.fiap.tech_challenge_ii.menu.core.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+}
