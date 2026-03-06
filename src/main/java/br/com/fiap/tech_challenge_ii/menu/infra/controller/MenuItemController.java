63c69e5ba9ebd5d812f28f1740e4c9cda735621e
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-05 21:22:16 -0300

    create menu item endpoint

--- src/main/java/br/com/fiap/tech_challenge_ii/menu/infra/controller/MenuItemController.java
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/infra/controller/MenuItemController.java
@@ -1,15 +1,22 @@
 package br.com.fiap.tech_challenge_ii.menu.infra.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.tech_challenge_ii.menu.core.dto.MenuItemDTO;
+import br.com.fiap.tech_challenge_ii.menu.core.dto.MenuItemRequestDTO;
+import br.com.fiap.tech_challenge_ii.menu.core.usecase.CreateMenuItemUseCase;
 import br.com.fiap.tech_challenge_ii.menu.core.usecase.FindMenuItemUseCase;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,8 +26,28 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/menu")
 public class MenuItemController {
 
-    // private final CreateMenuItemUseCase createMenuItemUseCase;
+    private final CreateMenuItemUseCase createMenuItemUseCase;
     private final FindMenuItemUseCase findMenuItemUseCase;
+
+    @PostMapping
+    public ResponseEntity<Map<String, List<Long>>> createMenuItems(
+            @Valid @RequestBody List<MenuItemRequestDTO> menuItems) {
+
+        List<MenuItemDTO> menuItemDTOs = menuItems.stream()
+                .map(req -> new MenuItemDTO(
+                        req.name(),
+                        req.description(),
+                        req.price(),
+                        req.isOnlyLocalConsuption(),
+                        req.photoPath(),
+                        req.restaurantId()))
+                .toList();
+
+        List<Long> ids = createMenuItemUseCase.save(menuItemDTOs);
+
+        URI uri = URI.create("/menu");
+        return ResponseEntity.created(uri).body(Map.of("ids", ids));
+    }
 
     @GetMapping("/{restaurantId}")
     public ResponseEntity<List<MenuItemDTO>> getMenuByRestaurant(
