fa5b63b8b87f8adcb17c00f91914eb6458fc0b8b
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-09 10:44:46 -0300

    create menu item not found exception

---
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/core/exception/MenuItemNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.fiap.tech_challenge_ii.menu.core.exception;
+
+public class MenuItemNotFoundException extends NotFoundException {
+
+    public MenuItemNotFoundException(String message) {
+        super(message);
+    }
+
+}
