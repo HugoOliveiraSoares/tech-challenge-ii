345f7c3985c05ab12e3709b587969ed0f7dbcc12
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-03-10 09:58:01 -0300

    update menu item controller

--- src/main/java/br/com/fiap/tech_challenge_ii/menu/infra/controller/MenuItemController.java
+++ src/main/java/br/com/fiap/tech_challenge_ii/menu/infra/controller/MenuItemController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,9 @@ import br.com.fiap.tech_challenge_ii.menu.core.dto.MenuItemDTO;
 import br.com.fiap.tech_challenge_ii.menu.core.usecase.CreateMenuItemUseCase;
 import br.com.fiap.tech_challenge_ii.menu.core.usecase.DeleteMenuItemUseCase;
 import br.com.fiap.tech_challenge_ii.menu.core.usecase.FindMenuItemUseCase;
+import br.com.fiap.tech_challenge_ii.menu.core.usecase.UpdateMenuItemUseCase;
 import br.com.fiap.tech_challenge_ii.menu.infra.controller.dto.MenuItemRequestDTO;
+import br.com.fiap.tech_challenge_ii.menu.infra.controller.dto.UpdateMenuItemRequestDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +35,7 @@ public class MenuItemController {
     private final CreateMenuItemUseCase createMenuItemUseCase;
     private final FindMenuItemUseCase findMenuItemUseCase;
     private final DeleteMenuItemUseCase deleteMenuItemUseCase;
+    private final UpdateMenuItemUseCase updateMenuItemUseCase;
 
     @PostMapping
     public ResponseEntity<Map<String, List<Long>>> createMenuItems(
@@ -72,6 +76,25 @@ public class MenuItemController {
         deleteMenuItemUseCase.delete(menuItemId, userId);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{restaurantId}/{menuItemId}")
+    public ResponseEntity<MenuItemDTO> updateMenuItem(
+            @RequestHeader("x-user-id") Long userId,
+            @PathVariable Long menuItemId,
+            @Valid @RequestBody UpdateMenuItemRequestDTO request) {
+
+        MenuItemDTO dto = new MenuItemDTO(
+                request.name(),
+                request.description(),
+                request.price(),
+                request.isOnlyLocalConsuption(),
+                request.photoPath(),
+                null);
+
+        MenuItemDTO updated = updateMenuItemUseCase.update(menuItemId, dto, userId);
+
+        return ResponseEntity.ok(updated);
     }
 
 }
